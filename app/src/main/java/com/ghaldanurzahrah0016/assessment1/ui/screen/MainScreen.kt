0219fd6c9ad1d6fc3156bf4994c797910d4a95b2
Ghalda Nur Zahrah <nurzahraghalda@gmail.com>
2026-04-07 08:57:36 +0700

    menambahkan inputan pengguna

--- app/src/main/java/com/ghaldanurzahrah0016/assessment1/ui/screen/MainScreen.kt
+++ app/src/main/java/com/ghaldanurzahrah0016/assessment1/ui/screen/MainScreen.kt
@@ -1,13 +1,22 @@
 package com.ghaldanurzahrah0016.assessment1.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -17,8 +26,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ghaldanurzahrah0016.assessment1.R
@@ -47,6 +61,16 @@ fun MainScreen() {
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier) {
     var nilai by remember { mutableStateOf("") }
+    var nilaierror by remember { mutableStateOf(false) }
+
+    val radioOption = listOf(
+        stringResource(R.string.inch),
+        stringResource(R.string.feet),
+        stringResource(R.string.pound),
+        stringResource(R.string.gallon)
+    )
+
+    var satuan by remember { mutableStateOf(radioOption[0]) }
 
     Column(
         modifier = modifier.fillMaxSize().padding(16.dp)
@@ -58,9 +82,69 @@ fun ScreenContent(modifier: Modifier = Modifier) {
         )
         OutlinedTextField(
             value = nilai,
-            onValueChange = { nilai = it }
-
+            onValueChange = { nilai = it },
+            label = { Text(text = stringResource(R.string.nilai)) },
+            trailingIcon = {IconPicker(nilaierror, "kg")},
+            supportingText = {ErrorHint(nilaierror)},
+            isError = nilaierror,
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
         )
+        Row (
+            modifier = Modifier
+                .padding(top = 6.dp)
+                .border(1.dp, Color.Gray, RoundedCornerShape(4.dp))
+        ){
+            radioOption.forEach { text ->
+                SatuanOption(
+                    label = text,
+                    isSelected = satuan == text,
+                    modifier = Modifier
+                        .selectable(
+                            selected = satuan == text,
+                            onClick = { satuan = text },
+                            role = Role.RadioButton
+                        )
+                        .weight(1f)
+                        .padding(16.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun SatuanOption(label: String, isSelected: Boolean, modifier: Modifier) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        RadioButton(selected = isSelected, onClick = null)
+        Text(
+            text = label,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(start = 8.dp)
+        )
+    }
+}
+
+@Composable
+fun IconPicker(isError: Boolean, unit: String) {
+    if (isError) {
+        Icon(imageVector = Icons.Filled.Warning, contentDescription = null)
+    } else {
+        Text(text = unit)
+    }
+}
+
+@Composable
+fun ErrorHint(isError: Boolean) {
+    if (isError) {
+        Text(text = stringResource(R.string.input_invalid))
     }
 }
 
